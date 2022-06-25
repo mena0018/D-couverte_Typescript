6b652d3224c4922ffe5974a9711b3786f1a1a49a
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-06-25 15:00:09 +0200

    Rectififcation dans les notions de base

--- rappel-cours/Les bases de TS/7. Interfaces/script.js
+++ rappel-cours/Les bases de TS/7. Interfaces/script.js
@@ -3,3 +3,18 @@
 //     price : number;
 //     carburant : number;
 // }
+// On peut implementer une Interface dans une classe
+class Space {
+    constructor(reactors, vMax, price, carburant) {
+        this.reactors = reactors;
+        this.vMax = vMax;
+        this.price = price;
+        this.carburant = carburant;
+        this.reactors = reactors;
+        this.vMax = vMax;
+        this.price = price;
+        this.carburant = carburant;
+    }
+}
+const falcon1 = new Space(2, 100, 100000, 10);
+console.log(falcon1);
--- rappel-cours/Les bases de TS/7. Interfaces/script.ts
+++ rappel-cours/Les bases de TS/7. Interfaces/script.ts
@@ -1,17 +1,22 @@
 interface Rocket {
     reactors: number;
     vMax: number;
-    takeOff: (action: string) =>  void
 }
 
 type Rocket2 = {
     reactors: number;
     vMax: number;
-    takeOff: (action: string) =>  void
+    takeOff: (action: string) =>  string
 }
 
 
-// La différence entre les 2 réside dans le fait qu'une interface peut être uniquement de type objet contrairement aux types.
+
+// Les interfaces peuvent être uniquement de type objet. on ne peut pas faire 
+// interface Exemple : string {
+// }
+// Contrairement aux Type
+type Exemple = string;
+
 
 // Également, avec une interface on peut faire du merging => rajouter des propriétés en cours de route
 interface Rocket {
@@ -24,3 +29,17 @@ interface Rocket {
 //     carburant : number;
 // }
 
+
+// On peut implementer une Interface dans une classe
+class Space implements Rocket {
+
+    constructor (public reactors: number, public vMax: number, public price: number, public carburant: number) { 
+        this.reactors = reactors;
+        this.vMax = vMax;
+        this.price = price;
+        this.carburant = carburant;
+    }
+}
+
+const falcon1 : Space = new Space(2, 100, 100000, 10)
+console.log(falcon1)
